1bf785b116ec7286aefbc089d4721a1e2a0e926f
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2025-11-10 15:51:08 +0000

    template para index

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
     return "Olá, mundo"
+
+@app.route("/")
+def index():
+    return redirect('/hello')
